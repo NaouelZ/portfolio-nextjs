b2b8460f65baaebf2c84975fbf58cdcaa6ca34bf
Naouel Zenati <zenati.naouel14@gmail.com>
2023-04-03 01:43:20 +0200

    tailwind fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
-  ],
-  purge: [
-    './Components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}'
+    "./components/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
